3076e6bd082092504ddeba9c0754ac3e5a2f8b59
Alexander Robertson <alexander.robertson@ed.ac.uk>
2018-09-05 18:10:36 +0100

    First commit!

---
+++ app.py
@@ -0,0 +1,40 @@
+import flask
+import pickle
+import pandas as pd
+
+# Use pickle to load in the pre-trained model.
+with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
+    model = pickle.load(f)
+
+app = flask.Flask(__name__, template_folder='templates')
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if flask.request.method == 'GET':
+        return(flask.render_template('main.html'))
+    
+    if flask.request.method == 'POST':
+        input_variables = flask.request.form.get('variables')
+
+        temperature = flask.request.form['temperature']
+        humidity = flask.request.form['humidity']
+        windspeed = flask.request.form['windspeed']
+
+        input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
+                                       columns=['temperature', 'humidity', 'windspeed'],
+                                       dtype=float,
+                                       index=['input'])
+
+        # input_variables = input_variables[['temperature', 'humidity', 'windspeed']]
+
+        prediction = model.predict(input_variables)[0]
+    
+        return flask.render_template('main.html',
+                                     original_input={'Temperature':temperature,
+                                                     'Humidity':humidity,
+                                                     'Windspeed':windspeed},
+                                     result=prediction,
+                                     )
+
+if __name__ == '__main__':
+    app.run()
