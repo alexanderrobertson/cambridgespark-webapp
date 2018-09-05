eac31ba61bb2bfd3e762981dda107cabefb37e28
alexanderrobertson <alexander.robertson@ed.ac.uk>
2018-09-05 19:06:28 +0100

    Added comments to code

--- app.py
+++ app.py
@@ -2,31 +2,37 @@ import flask
 import pickle
 import pandas as pd
 
-# Use pickle to load in the pre-trained model.
+# Use pickle to load in the pre-trained model
 with open(f'model/bike_model_xgboost.pkl', 'rb') as f:
     model = pickle.load(f)
 
+# Initialise the Flask app
 app = flask.Flask(__name__, template_folder='templates')
 
+# Set up the main route
 @app.route('/', methods=['GET', 'POST'])
 def main():
     if flask.request.method == 'GET':
+        # Just render the initial form, to get input
         return(flask.render_template('main.html'))
     
     if flask.request.method == 'POST':
-        input_variables = flask.request.form.get('variables')
-
+        # Extract the input
         temperature = flask.request.form['temperature']
         humidity = flask.request.form['humidity']
         windspeed = flask.request.form['windspeed']
 
+        # Make DataFrame for model
         input_variables = pd.DataFrame([[temperature, humidity, windspeed]],
                                        columns=['temperature', 'humidity', 'windspeed'],
                                        dtype=float,
                                        index=['input'])
 
+        # Get the model's prediction
         prediction = model.predict(input_variables)[0]
     
+        # Render the form again, but add in the prediction and remind user
+        # of the values they input before
         return flask.render_template('main.html',
                                      original_input={'Temperature':temperature,
                                                      'Humidity':humidity,
